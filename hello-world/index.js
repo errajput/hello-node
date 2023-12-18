dd7ebffdbf91d3537f0a3a3e74365a4743fdf80c
errajput <dishacsevira@gmail.com>
2023-12-18 21:57:00 +0530

    add alt, ctrl, shift

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+console.log("Hello world");
+
+// const sum = require('./utils');
+// console.log(sum(1, 2));
+
+import { sum } from "./utils.js";
+
+console.log(sum(1, 2));
