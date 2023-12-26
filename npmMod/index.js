38aaa9a71f5031dffc4699ada95bc1d43e80e69b
errajput <dishacsevira@gmail.com>
2023-12-26 21:57:11 +0530

    add npm

--- npmMod/index.js
+++ npmMod/index.js
@@ -2,10 +2,10 @@ import chalk from "chalk";
 const log = console.log;
 log(chalk.blue("Hello") + " World" + chalk.red("!"));
 
-// const validator = require("validator");
-// //console.log(chalk.blue.underline.inverse("false"));
+import validator from "validator";
+//console.log(chalk.blue.underline.inverse("false"));
 
-// const res = validator.isEmail("dishacsevira@gmail.monkkl");
-// console.log(
-//   res ? chalk.green.inverse(res) : chalk.foregroundColorNames.inverse(res)
-// );
+const res = validator.isEmail("dishacsevira@gmail.monkkl");
+console.log(
+  res ? chalk.green.inverse(res) : chalk.foregroundColorNames.inverse(res)
+);
