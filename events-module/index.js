dde5040a4db6730fd8e4314477777617f4880b2b
errajput <dishacsevira@gmail.com>
2023-12-29 22:03:28 +0530

    add  events

--- events-module/index.js
+++ events-module/index.js
@@ -13,7 +13,7 @@ const event = new EventEmitter();
 //   console.log("your name is disha rajput 4");
 // });
 event.on("checkPage", (sc, msg) => {
-  console.log("status code is $(sc) and the page is $(mag)");
+  console.log("status code is $(sc) and the page is ($msg)");
 });
 
 event.emit("checkPage", 200, "ok");
